ec55b87b924296e6929b8e6b90b418d371783ef3
Owen Liu <owenliu0924useful@gmail.com>
2026-02-22 21:31:14 +0800

    Initial commit

---
+++ PrismatiXEngine/PrismatiXEngine.cpp
@@ -0,0 +1,23 @@
+﻿#include <SDL2/SDL.h>
+#include <iostream>
+#include "PrismatiXEngine.h"
+
+int main(int argc, char* argv[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* window = SDL_CreateWindow("PrismatiX VN Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+
+	bool isRunning = true;
+
+	SDL_Event event;
+	while (isRunning) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				std::cout << "Closing.." << std::endl;
+				isRunning = false;
+			}
+		}
+	}
+
+	return 0;
+}
