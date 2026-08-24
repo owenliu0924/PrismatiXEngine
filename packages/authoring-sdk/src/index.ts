eeeb837c883439737d36897ba495426e6f622455
Owen Liu <64713824+owenliu0924@users.noreply.github.com>
2026-08-25 02:10:59 +0800

    feat: export project story compiler

--- packages/authoring-sdk/src/index.ts
+++ packages/authoring-sdk/src/index.ts
@@ -3,4 +3,5 @@ export * from "./contracts.js";
 export * from "./localization.js";
 export * from "./semantic.js";
 export * from "./story.js";
+export * from "./story-project.js";
 export type * from "./types.js";
