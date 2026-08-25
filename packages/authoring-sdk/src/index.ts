280657d49ee12207cb56f754a356f18c32cba613
Owen Liu <64713824+owenliu0924@users.noreply.github.com>
2026-08-25 14:22:15 +0800

    feat: export resource identity helpers

--- packages/authoring-sdk/src/index.ts
+++ packages/authoring-sdk/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./canonical-json.js";
 export * from "./contracts.js";
 export * from "./localization.js";
+export * from "./resources.js";
 export * from "./semantic.js";
 export * from "./story.js";
 export * from "./story-project.js";
